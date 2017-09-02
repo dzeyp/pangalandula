a85a3f90655f05117b26b60f44a27846e51fc0eb
Julius <jaypee.kans@gmail.com>
2017-09-02 21:17:57 +0400

    Weekend update

--- app.js
+++ app.js
@@ -4,10 +4,12 @@
 	var gameId = 0;
 
 	function newGame() {
+		document.getElementById("joinIntro").play();
 		$.ajax({
-			url: "aaaaaaaaaaaaaaaaaa/newgame"
+			url: "https://joinup.tv/api/newgame"
 		})
 		.done(function( result ) {
+			$( "#room-code" ).empty();
 			$( "#room-code" ).append( result.gameCode );
 			gameId = result.gameId;
 			joinLong();
@@ -16,19 +18,17 @@
 
 	function joinLong() {
 		$.ajax({
-			url: "aaaaaaaaaaaaaaaaaa/game2/" + gameId
+			url: "https://joinup.tv/api/game2/" + gameId
 		})
 		.done(function( result ) {
-			console.log( result );
 			if ( result !== 'timeout' ) {
 				var gameObj = JSON.parse( result );
+				$( ".row-player" ).remove();
 				gameObj.game.users.forEach(function( user ) {
 					if ( user.team == 1 ) {
-						$( "#" + user.name ).remove();
-						$( "#red-team" ).append( '<h5 id="' + user.name + '">' + user.name + '</h5>' );
+						$( "#red-team" ).find( ".row" ).append( '<div class="col-12 row-player"><h5 class="player-name-red-team" id="' + user.name + '">' + user.name + '</h5></div>' );
 					} else {
-						$( "#" + user.name ).remove();
-						$( "#blue-team" ).append( '<h5 id="' + user.name + '">' + user.name + '</h5>' );
+						$( "#blue-team" ).find( ".row" ).append( '<div class="col-12 row-player"><h5 class="player-name-blue-team" id="' + user.name + '">' + user.name + '</h5></div>' );
 					}
 				});
 
@@ -39,17 +39,46 @@
 			}
 
 			if ( hasStarted ) {
-				getTemplate( "waitplayers" );
-				joinShort();
+				joinShortInit();
 			} else {
 				joinLong();
 			}
 		})
 	}
 
+	function joinShortInit() {
+		getTemplate( "waitplayers" );
+		document.getElementById("joinIntro").pause();
+		document.getElementById("waitingIntro").play();
+		$.ajax({
+			url: "https://joinup.tv/api/game/" + gameId
+		})
+		.done(function( result ) {
+			result.game.users.forEach(function( user ) {
+				if ( user.team == 1) {
+					$( "#waiting-red-team" ).append( '<div class="col-12 row-player"><h5 class="player-name-red-team" id="' + user.name + '">' + user.name + '</h5></div>' );
+				} else {
+					$( "#waiting-blue-team" ).append( '<div class="col-12 row-player"><h5 class="player-name-blue-team" id="' + user.name + '">' + user.name + '</h5></div>' );
+				}
+			});
+
+			var maxNamesNum = userNum * 5;
+			var currentNamesNum = result.game.names.length;
+			var progress = ( currentNamesNum / maxNamesNum ) * 100;
+
+			$( "#progress" ).text( Math.round(progress) );
+
+			if ( maxNamesNum <= currentNamesNum ) {
+				playerInstruction();
+			} else {
+				joinShort();
+			}
+		})
+	}
+
 	function joinShort() {
 		$.ajax({
-			url: "aaaaaaaaaaaaaaaaaa/game/" + gameId
+			url: "https://joinup.tv/api/game/" + gameId
 		})
 		.done(function( result ) {
 			var maxNamesNum = userNum * 5;
@@ -68,27 +97,61 @@
 
 	function playerInstruction() {
 		getTemplate( "playerinstruction" );
-		document.getElementById("waitingIntro").play();
-
-		setTimeout(playerSelect, 40000);
+		document.getElementById("waitingIntro").pause();
+		document.getElementById("round1Rules").play();
+		setTimeout(playerSelect, 27000);
 	}
 
 	function playerSelect() {
-		document.getElementById("waitingIntro").pause();
 		getTemplate( "playerselect" );
 
 		$.ajax({
-			url: "aaaaaaaaaaaaaaaaaa/currentuser/" + gameId
+			url: "https://joinup.tv/api/currentuser/" + gameId
 		})
 		.done(function( result ) {
-			$( "#currentPlayerName" ).text( result.name );
-			clockReady( result.userid );
+			document.getElementById("nextPlayer").play();
+			$( "#current-team" ).empty();
+			$( ".current-player" ).removeClass( "player-name-red-team" );
+			$( ".current-player" ).removeClass( "player-name-blue-team" );
+			$( ".current-player" ).text( '' );
+
+			if ( result.team == 1) {
+				$( "#current-team" ).append( '<img class="img-text-teams mb-3" src="img/join_screen/red-team.png" alt="red team">' );
+				setTimeout(function() {
+					$( ".current-player" ).addClass( "player-name-red-team" );
+					$( ".current-player" ).text( result.name );
+				}, 6000);
+			} else {
+				$( "#current-team" ).append( '<img class="img-text-teams mb-3" src="img/join_screen/blue-team.png" alt="blue team">' );
+				setTimeout(function() {
+					$( ".current-player" ).addClass( "player-name-blue-team" );
+					$( ".current-player" ).text( result.name );
+				}, 6000);
+			}
+
+			setTimeout(function() {
+				clockReady( result.userid );
+			}, 12000);
 		})
 	}
 
+	/*function nextPlayer() {
+		getTemplate( "playerselect" );
+
+		$.ajax({
+			url: "https://joinup.tv/api/currentuser/" + gameId
+		})
+		.done(function( result ) {
+			$( "#currentPlayerName" ).text( result.name );
+			setTimeout(function() {
+				clockReady( result.userid );
+			}, 12000);
+		})
+	}*/
+
 	function clockReady( userId ) {
 		$.ajax({
-			url: "aaaaaaaaaaaaaaaaaa/clockready/" + gameId + "/" + userId
+			url: "https://joinup.tv/api/clockready/" + gameId + "/" + userId
 		})
 		.done(function( result ) {
 			playerStart( userId );
@@ -96,37 +159,76 @@
 	}
 
 	function playerStart( userId ) {
+		$( '#playerProgress' ).text( 30 );
 		getTemplate( "playerstart" );
 
 		$.ajax({
-			url: "aaaaaaaaaaaaaaaaaa/clock/" + gameId
+			url: "https://joinup.tv/api/clock/" + gameId
 		})
 		.done(function( result ) {
 			if ( result !== 'timeout' ) {
 				var clockObj = JSON.parse( result );
 				
 				if ( clockObj.start ) {
-					//start counting
-					recordAnswers( userId );
-					//stop after timer reaches 0 (timeout api)
-					//use lastturn api to check answers
+					startTimer( userId );
 				}
+			} else {
+				playerStart( userId );
 			}
 		})
 	}
 
-	function tally( userId ) {
+	function startTimer( userId, max = 30 ) {
+		setTimeout(function() {
+	        $( '#playerProgress' ).text( max );
+
+	        if ( max == 0 ) {
+	        	stopTimer( userId );
+	        } else {
+	        	startTimer( userId, --max );
+	        }
+	    }, 1000);
+	}
+
+	function stopTimer( userId ) {
 		$.ajax({
-			url: "aaaaaaaaaaaaaaaaaa/lastturn/" + gameId + "/" + userId
+			url: "https://joinup.tv/api/timeout/" + gameId + "/" + userId
 		})
 		.done(function( result ) {
-			result.lastTurn
+			tally( userId );
 		})
 	}
 
-	function currentUser() {
-		
+	function tally( userId ) {
+		getTemplate( "tally" );
+
+		$.ajax({
+			url: "https://joinup.tv/api/lastturn/" + gameId + "/" + userId
+		})
+		.done(function( result ) {
+			var score = 0;
+			
+			$( ".score-player" ).empty();
+			$( ".score-names" ).empty();
+
+			result.lastTurn.names.forEach(function( name ) {
+				if ( name.status == "correct" ) {
+					$( ".score-names" ).append( "<h5>" + name.name + "</h5>" );
+					score++;
+				}
+			});
+
+			$( ".score-player" ).append( "<h3>" + result.lastTurn.name + "</h3>" );
+			$( ".score-player" ).append( "<h4>" + score + "</h4>" );
+
+			setTimeout(playerSelect, 5000);
+		})
 	}
+
+
+
+
+
 
 	function getTemplate( $name ) {
 		$( ".block" ).hide();
--- index.php
+++ index.php
@@ -23,33 +23,62 @@
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,700" rel="stylesheet" type="text/css">
 
         <style>
+            @font-face {
+                font-family: "Phagspa";
+                src: url("font/phagspa-bold.ttf");
+            }
             body {
                 font-family: 'Phagspa', "Open Sans";
-                background: #A91B28;
+                background: #347595;
                 color: #ffffff;
             }
 
             img {
-                max-width: 100%;
+                width: 100%;
+            }
+
+            .logo {
+                max-width: 220px;
+            }
+
+            .join-screen .img-text-players,
+            .join-screen .img-text-teams,
+            .playerselect .img-text-teams {
+                max-width: 220px;
+            }
+
+            .game-code-box {
+                background-position: center;
+                background-size: 130px;
+                background-image: url(img/join_screen/game-code-box.png);
+                background-repeat: no-repeat;
+            }
+
+            .player-name-red-team,
+            .player-name-blue-team {
+                width: 100%;
+                display: inline-block;
+                background-position: center;
+                background-size: 100%;
+                background-repeat: no-repeat;
+                max-width: 220px;
+                line-height: 40px;
+            }
+
+            .player-name-red-team {
+                background-image: url(img/join_screen/red-box-3.png);
+            }
+
+            .player-name-blue-team {
+                background-image: url(img/join_screen/blue-box-3.png);
             }
 
             .toolbar {
                 height: 30px;
             }
 
-            #room-code {
-                -webkit-border-radius: 30px;
-                -moz-border-radius: 30px;
-                border-radius: 20px;
-                border: 2px solid #000000;
-                width: 150px;
-                display: inline-block;
-                background: green;
-                padding: 10px 0;
-            }
-
-            h1, h2, h3, h4, h5, h6 {
-                font-weight: 700;
+            h1, h2, h3, h4, h5, h6, p {
+                text-transform: uppercase;
             }
 
             .container {
@@ -63,68 +92,163 @@
             .hidden {
                 display: none;
             }
+
+            .bg-red {
+                background: #A91B28;
+            }
+
+            .bg-blue {
+                background: #347595;
+            }
+
+            .bg-blue2 {
+                background: blue;
+            }
+
+            .set-middle {
+                display: flex;
+                flex-direction: column;
+                flex: 1;
+            }
+
+            .center-middle {
+                margin: auto;
+            }
+
+            #room-code {
+                padding: 15px 30px;
+            }
+
+            .clearfix {
+                clear: both;
+            }
+
+            .joinup-tv-intro {
+                max-width: 310px;
+            }
         </style>
     </head>
     <body>
         <!--[if lte IE 9]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
         <![endif]-->
-        <div class="container-fluid block newgame">
-            <div class="row toolbar">
-                <div class="col-12"></div>
-            </div>
+        <div class="container-fluid block join-screen bg-red pb-3 pt-3">
             <div class="row">
-                <div class="col-4 text-center">
-                    <img src="https://joinup.tv/img/ng-logo-man.png" alt="Name Game logo">
-                    <img src="https://joinup.tv/img/ng-logo-text.png" alt="Name Game text logo">
+                <div class="col-4 col-md-6 text-center">
+                    <img class="logo" src="img/join_screen/logo-man.png" alt="Name Game logo">
+                    <img class="logo" src="img/join_screen/logo-name.png" alt="Name Game text logo">
                 </div>
-                <div class="col-8 text-center">
-                    <h6>GO TO JOINUP.TV AND ENTER THE ROOM CODE BELOW</h6>
-                    <h2 id="room-code"></h2>
+                <div class="col-8 col-md-6 text-center set-middle">
+                    <div class="set-middle">
+                        <h5 class="center-middle joinup-tv-intro mb-3">GO TO JOINUP.TV AND ENTER THE ROOM CODE BELOW</h5>
+                        <div class="center-middle game-code-box mt-1">
+                            <h2 id="room-code" class="center-middle">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h2>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="container-fluid block newgame">
+
+        <div class="container-fluid block join-screen pt-3">
             <div class="row">
-                <div class="col-12 text-center">
-                    <h3>PLAYERS</h3>
+                <div class="col-12 text-center mb-3">
+                    <img class="img-text-players" src="img/join_screen/players.png" alt="players">
                 </div>
             </div>
             <div class="row">
                 <div class="col-6 text-center" id="red-team">
-                    <h4>RED TEAM</h4>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-12 text-center">
+                                <img class="img-text-teams mb-3" src="img/join_screen/red-team.png" alt="players">
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-6 text-center" id="blue-team">
-                    <h4>BLUE TEAM</h4>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-12 text-center">
+                                <img class="img-text-teams mb-3" src="img/join_screen/blue-team.png" alt="players">
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
 
-        <div class="container-fluid block waitplayers hidden">
+        <div class="container-fluid block waitplayers pt-3 hidden">
+            <div class="row">
+                <div class="col-12 col-sm-4 text-center">
+                    <img class="logo mb-3" src="img/waiting_screen/thinking_man.png" alt="Thinking Man">
+                </div>
+                <div class="col-12 col-sm-8 text-center">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="container-fluid">
+                                    <div class="row" id="waiting-red-team">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="container-fluid">
+                                    <div class="row" id="waiting-blue-team">
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 text-center">
+                    progress bar here
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid block playerinstruction pt-3 hidden">
             <div class="row">
                 <div class="col-12 text-center">
-                    <h5>Loading: <span id="progress">0</span>%</h5>
+                    round one
+                    ok everyone
+                    now the name of the game
+                    is to have your team
+                    guess as many names as possible
+                    in under 30 seconds
+                    without saying the name!
+                    in this round
+                    you can say anything you like to describe the name
+                    but you can't say
+                    what the name rhymes with
+                    make sure you don't press next
+                    until you hear a member of your team
+                    say the name
+                    exactly as it appears on you phone
+                    all clear?
                 </div>
             </div>
         </div>
 
-        <div class="container-fluid block playerinstruction hidden">
+        <div class="container-fluid block playerselect pt-3 hidden">
             <div class="row">
-                <div class="col-12 text-center">
-                    <h5>Instructions Text Here</h5>
+                <div class="col-12 col-sm-4 text-center">
+                    <img class="logo mb-3" src="img/nextup_screen/boxing-lady.png" alt="Thinking Man">
+                </div>
+                <div class="col-12 col-sm-8 text-center">
+                    <h5>Alright then, let battle commence! next up...</h5>
+                    <div id="current-team">
+                        <!-- <img class="img-text-teams mb-3" src="img/join_screen/red-team.png" alt="players"> -->
+                    </div>
+                    <div class="clearfix"></div>
+                    <h5 class="current-player"></h5>
+                    <h5>Please stand up, turn around and address your team mates.</h5>
                 </div>
             </div>
         </div>
 
-        <div class="container-fluid block playerselect hidden">
-            <div class="row">
-                <div class="col-12 text-center">
-                    <h5>Next player: <span id="currentPlayerName"></span></h5>
-                </div>
-            </div>
-        </div>
-
-        <div class="container-fluid block playerstart hidden">
+        <div class="container-fluid block playerstart pt-3 hidden">
             <div class="row">
                 <div class="col-12 text-center">
                     <h5>Timer: <span id="playerProgress">30</span></h5>
@@ -132,8 +256,18 @@
             </div>
         </div>
 
-        <script src="app.js"></script>
+        <div class="container-fluid block tally pt-3 hidden">
+            <div class="row">
+                <div class="col-6 text-center score-player"></div>
+                <div class="col-6 text-center score-names"></div>
+            </div>
+        </div>
 
-        <audio id="waitingIntro" src="" autostart="false" ></audio>
+        <audio id="joinIntro" src="https://d3en5xriihnzfd.cloudfront.net/game_music/screens/player_join/intro.mp3" autostart="false"></audio>
+        <audio id="waitingIntro" src="https://d3en5xriihnzfd.cloudfront.net/game_music/screens/waiting/intro.mp3" autostart="false"></audio>
+        <audio id="round1Rules" src="https://d3en5xriihnzfd.cloudfront.net/game_music/screens/round1/rules.mp3" autostart="false"></audio>
+        <audio id="nextPlayer" src="https://d3en5xriihnzfd.cloudfront.net/game_music/screens/next_up/intro/next/1_1.mp3" autostart="false"></audio>
+
+        <script src="app.js"></script>
     </body>
 </html>
